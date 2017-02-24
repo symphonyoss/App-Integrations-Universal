c09575b4e24455f0927d78d4df5d08515c334ef9
Robson Vinícius Vieira Sanchez <rsanchez@symphony.com>
2017-02-24 11:35:35 -0300

    APP-705 Integration-commons Remove Symphony dependencies

--- src/test/java/org/symphonyoss/integration/webhook/universal/SimpleWebHookIntegrationTest.java
+++ src/test/java/org/symphonyoss/integration/webhook/universal/SimpleWebHookIntegrationTest.java
@@ -18,14 +18,10 @@ package org.symphonyoss.integration.webhook.universal;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
 import org.symphonyoss.integration.entity.MessageMLParseException;
-import org.symphonyoss.integration.entity.MessageMLParser;
 import org.symphonyoss.integration.webhook.WebHookPayload;
 import org.symphonyoss.integration.webhook.exception.WebHookParseException;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -36,11 +32,6 @@ import java.util.Map;
  */
 public class SimpleWebHookIntegrationTest {
 
-  private static final String INVALID_MESSAGEML = "<messageML><mention email=\"rsanchez@symphony"
-      + ".com\"/> created SAM-25 (<a href=\"https://whiteam1.atlassian.net/browse/SAM-25\"/>) "
-      + "(<b>Highest Story in Sample 1 with labels &quot;production&quot;)<br>Description: Issue "
-      + "Test<br>Assignee: <mention email=\"rsanchez@symphony.com\"/></messageML>";
-
   private static final String VALID_MESSAGEML = "<messageML><mention email=\"rsanchez@symphony"
       + ".com\"/> created SAM-25 (<a href=\"https://whiteam1.atlassian.net/browse/SAM-25\"/>) "
       + "(<b>Highest</b> Story in Sample 1 with labels &quot;production&quot;)<br/>Description: Issue "
@@ -48,32 +39,9 @@ public class SimpleWebHookIntegrationTest {
 
   private SimpleWebHookIntegration simpleWebHookIntegration = new SimpleWebHookIntegration();
 
-  private MessageMLParser parser = new MessageMLParser();
-
-  @Before
-  public void setup() {
-    this.parser.init();
-    ReflectionTestUtils.setField(simpleWebHookIntegration, "parser", parser);
-  }
-
-  @Test(expected = MessageMLParseException.class)
-  public void testInvalidBody() throws WebHookParseException {
-    WebHookPayload payload = new WebHookPayload(Collections.<String, String>emptyMap(), Collections.<String, String>emptyMap(), INVALID_MESSAGEML);
-    simpleWebHookIntegration.parse(payload);
-  }
-
   @Test(expected = MessageMLParseException.class)
   public void testEmptyForm() throws WebHookParseException {
     WebHookPayload payload = new WebHookPayload(Collections.<String, String>emptyMap(), Collections.<String, String>emptyMap(), null);
-    simpleWebHookIntegration.parse(payload);
-  }
-
-  @Test(expected = MessageMLParseException.class)
-  public void testInvalidForm() throws WebHookParseException {
-    Map<String, String> parameters = new HashMap<>();
-    parameters.put(SimpleWebHookIntegration.PAYLOAD, INVALID_MESSAGEML);
-
-    WebHookPayload payload = new WebHookPayload(parameters, Collections.<String, String>emptyMap(), null);
     simpleWebHookIntegration.parse(payload);
   }
 
