4a07985fb8bc611f3c262b836dfa7d5e2fba80d9
pdarde-daitan <pdarde@daitangroup.com>
2017-04-20 14:25:30 -0300

    APP-952, fix header text

--- src/main/webapp/js/config.service.js
+++ src/main/webapp/js/config.service.js
@@ -1,6 +1,6 @@
 export default ({
-  appTitle: 'Configure Universal Webhook Integration',
-  appName: 'Universal Webhook Integration',
+  appTitle: 'Universal',
+  appName: 'Universal',
   appSubTitle: 'Universal Webhook Integration',
   appIMShortHand: 'Universal Webhook', // will display "One on One with Universal Webhook"
   appToggleSetupInstructions: true,
