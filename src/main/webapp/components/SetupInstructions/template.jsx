d12a2fa8ef5bc07a6a4d2f11d1de4976e83063e4
Robson Vinícius Vieira Sanchez <rsanchez@symphony.com>
2017-11-07 17:37:49 -0200

    APP-1454 Update universal webhook integration - link for documentation

--- src/main/webapp/components/SetupInstructions/template.jsx
+++ src/main/webapp/components/SetupInstructions/template.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Instructions = () => (
   <div>
-    <p>Refer to the <a href='https://documentation.symphony.com/documents/universal_webhook_integration/universal_webhook_integration_1.43.0.0.pdf' target='blank'>Universal Webhook</a> document to learn more how to build your own webhook.</p>
+    <p>Refer to the <a href='https://github.com/symphonyoss/App-Integrations-Universal/blob/dev/README.md' target='blank'>Universal Webhook</a> documentation to learn how to build your own webhook.</p>
   </div>
 );
 
